b6d2ba373fa8ea81e06213afff5be4e4da5cac7c
jeremiedb <jeremie_db@hotmail.com>
2020-01-07 00:13:37 -0500

    MLJ tests

--- test/MLJ.jl
+++ test/MLJ.jl
@@ -91,9 +91,18 @@ tree_model = EvoTreeRegressor(
     rowsample=0.5, colsample=0.5, nbins=32)
 
 X = Tables.table(X)
+X = Tables.rowtable(X)
+X_matrix = MLJBase.matrix(X)
+@time X = DataFrame(X)
+# typeof(X)
+# @time Xmatrix = MLJBase.matrix(X)
+
+# typeof(X)
 @time tree = machine(tree_model, X, Y)
 train, test = partition(eachindex(Y), 0.8, shuffle=true); # 70:30 split
 @time MLJ.fit!(tree, rows=train, verbosity=1, force=true)
+@time EvoTrees.grow_gbtree_MLJ(X_matrix, Y, tree_model, verbosity=1)
+@time EvoTrees.grow_gbtree(X_matrix, Y, tree_model, verbosity=1)
 
 tree.model.nrounds = 10
 tree.cache.params.nrounds = 10
@@ -104,9 +113,81 @@ tree.model.nrounds += 10
 
 tree.model.nrounds += 10
 @time MLJBase.update(tree.model, 0, tree.fitresult, tree.cache, X, Y)
+# @time x1, x2, x3 = MLJBase.update(tree.model, 0, tree.fitresult, tree.cache, X, Y)
 
-tree.model.nrounds += 1
-@time MLJ.fit!(tree, rows=train, verbosity=0)
+tree.model.nrounds += 10
+@time MLJ.fit!(tree, rows=train, verbosity=1)
+# @time MLJBase.fit!(tree, rows=train, verbosity=1)
+
+tree isa NodalMachine
+
+function test_fit!(mach::AbstractMachine; rows=nothing, verbosity=1, force=false)
+
+    if mach isa NodalMachine && mach.frozen
+        verbosity < 0 || @warn "$mach not trained as it is frozen."
+        return mach
+    end
+
+    warning = clean!(mach.model)
+    isempty(warning) || verbosity < 0 || @warn warning
+
+    if rows === nothing
+        rows = (:)
+    end
+
+    rows_have_changed = !isdefined(mach, :previous_rows) ||
+        rows != mach.previous_rows
+
+    if mach isa NodalMachine
+        # determine if concrete data to be used in training may have changed:
+        upstream_state = Tuple([state(arg) for arg in mach.args])
+        data_has_changed =
+            rows_have_changed || (upstream_state != mach.upstream_state)
+        previously_fit = (mach.state > 0)
+        args = [arg(rows=rows) for arg in mach.args]
+    else
+        data_has_changed = rows_have_changed
+        previously_fit = isdefined(mach, :fitresult)
+        # args = [selectrows(arg, rows) for arg in mach.args]
+        # println(rows_have_changed)
+        args = rows_have_changed ? [selectrows(arg, rows) for arg in mach.args] : mach.args
+        # args = mach.args
+    end
+
+    if !previously_fit || data_has_changed || force
+        # fit the model:
+        verbosity < 1 || @info "Training $mach."
+        mach.fitresult, mach.cache, mach.report =
+            fit(mach.model, verbosity, args...)
+
+    elseif !MLJBase.is_stale(mach)
+        # don't fit the model
+        if verbosity > 0
+            @info "Not retraining $mach.\n It appears up-to-date. " *
+                  "Use `force=true` to force retraining."
+        end
+        return mach
+    else
+        # update the model:
+        verbosity < 1 || @info "Updating $mach."
+        mach.fitresult, mach.cache, mach.report =
+            MLJBase.update(mach.model, verbosity, mach.fitresult, mach.cache, args...)
+    end
+
+    if rows_have_changed
+        mach.previous_rows = deepcopy(rows)
+    end
+
+    mach.previous_model = deepcopy(mach.model)
+
+    if mach isa NodalMachine
+        mach.upstream_state = upstream_state
+         mach.state = mach.state + 1
+    end
+
+    return mach
+end
+
 
 @time for i in 1:10
     tree.model.nrounds += 1
@@ -117,6 +198,27 @@ end
     tree.model.nrounds += 10
     MLJ.fit!(tree, rows=train, verbosity=0)
 end
+
+@time for i in 1:10
+    tree.model.nrounds += 1
+    test_fit!(tree, rows=train, verbosity=0)
+end
+
+@time for i in 1:1
+    tree.model.nrounds += 10
+    test_fit!(tree, rows=train, verbosity=0)
+end
+
+
+@time for i in 1:10
+    tree.model.nrounds += 1
+    MLJBase.update(tree.model, 0, tree.fitresult, tree.cache, X, Y)
+end
+@time for i in 1:1
+    tree.model.nrounds += 10
+    MLJBase.update(tree.model, 0, tree.fitresult, tree.cache, X, Y)
+end
+
 
 # yhat = MLJBase.predict(tree.model, tree.fitresult, MLJ.selectrows(X,test))
 pred_train = MLJ.predict(tree, MLJ.selectrows(X,train))
