b4803e2032e14eeb9227ae5878e3b17133757419
jeremiedb <jeremie_db@hotmail.com>
2020-02-04 19:08:59 -0500

    test kernel

--- test/GPU.jl
+++ test/GPU.jl
@@ -1,98 +1,48 @@
 # using CUDA
 using CUDAnative
 using CuArrays
-using Flux
-using GeometricFlux
+# using Flux
+# using GeometricFlux
 
-features = rand(1_000, 10)
-features_int = rand(UInt8, 1_000, 10)
+# CPU
+function hist_cpu!(hist, δ, idx)
+    Threads.@threads for j in 1:size(idx,2)
+        @inbounds for i in 1:size(idx,1)
+            hist[idx[i], j] += δ[i,j]
+        end
+    end
+    return
+end
+
+# GPU
+function hist_gpu!(h::CuMatrix{T}, x::CuArray{T}, id::CuArray{Int}; MAX_THREADS=256) where {T<:AbstractFloat}
+    function kernel!(h::CuDeviceArray{T}, x::CuDeviceArray{T}, id)
+        i = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+        j = threadIdx().y + (blockIdx().y - 1) * blockDim().y
+        @inbounds if i <= size(id, 1) && j <= size(h, 2)
+            k = Base._to_linear_index(h, id[i,j], j)
+            CUDAnative.atomic_add!(pointer(h, k), x[i,j])
+        end
+        return
+    end
+    thread_i = min(MAX_THREADS, size(id, 1))
+    thread_j = min(MAX_THREADS ÷ thread_i, size(h, 2))
+    threads = (thread_i, thread_j)
+    blocks = ceil.(Int, (size(id, 1), size(h, 2)) ./ threads)
+    CuArrays.@cuda blocks=blocks threads=threads kernel!(h, x, id)
+    return h
+end
 
 nbins = 20
-hist = zeros(Float32, 1, nbins)
-δ = rand(Float32, 1, 10_000_000)
-δ² = rand(Float32, 1, 10_000_000)
-idx = rand(1:nbins, 10_000_000)
-sub_idx = rand(1:nbins, 100_000)
-set = collect(1:length(idx))
-sub_set = sort(rand(set, 100_000))
+ncol = 100
+items = Int(1e6)
+hist = zeros(Float32, nbins, ncol)
+δ = rand(Float32, items, ncol)
+idx = rand(1:nbins, items, ncol)
 
 hist_gpu = CuArray(hist)
 δ_gpu = CuArray(δ)
-δ²_gpu = CuArray(δ²)
 idx_gpu = CuArray(idx)
-set_gpu = CuArray(set)
 
-@time sumpool(idx, δ)
-@time sumpool(idx_gpu, δ_gpu)
-@time scatter_add!(hist, δ, idx)
-
-function scatter_loop(hist, δ, idx)
-    for i in 1:10
-        scatter_add!(hist, δ, idx)
-    end
-    return Array(hist)
-end
-@time hh = scatter_loop(hist_gpu, δ_gpu, idx_gpu)
-println(hh)
-CuArrays.@time scatter_add!(hist_gpu, δ_gpu, idx_gpu)
-
-
-δ = rand(Float32, 100, 2_000_000)
-δ² = rand(Float32, 100, 2_000_000)
-δ_gpu = CuArray(δ)
-δ²_gpu = CuArray(δ²)
-CuArrays.@time CuArrays.scan!(+ ,δ²_gpu, δ_gpu,dims=1)
-@time x = δ²_gpu[2,5]
-
-δ = rand(Float32, 1, 1_000)
-hist_gpu = CuArray(hist)
-δ_gpu = CuArray(δ[1:1000])
-idx_gpu = CuArray(idx[1:1000])
-scatter_add!(hist_gpu, δ_gpu, idx_gpu)
-sumpool(idx[1:1000], δ)
-
-function split_cpu(hist, δ, idx, set)
-    @inbounds for i in set
-        hist[idx[i]] += δ[i]
-    end
-    return
-end
-
-function split_gpu(hist, δ, idx, set)
-    @inbounds for i in set
-        hist[idx[i]] += δ[i]
-    end
-    return
-end
-
-hist
-@time split_cpu(hist, δ, idx, set)
-@time split_gpu(hist, δ_gpu, idx, set)
-
-function gpu_add2!(y, x)
-    index = threadIdx().x
-    stride = blockDim().x
-    @inbounds for i = index:stride:length(y)
-        @inbounds y[i] += x[i]
-    end
-    return nothing
-end
-
-δ = rand(Float32, 1_000_000)
-δ² = rand(Float32, 1_000_000)
-δ_gpu = CuArray(δ)
-δ²_gpu = CuArray(δ²)
-CuArrays.@time @cuda threads=256 gpu_add2!(δ_gpu, δ²_gpu)
-
-
-function hist_test!(hist, idx, x)
-    index = threadIdx().x
-    stride = blockDim().x
-    for i = index:stride:length(x)
-        hist[idx[i]] += x[i]
-    end
-    return nothing
-end
-
-hist = zeros(Float32, nbins)
-@time @cuda threads=256 hist_test!(hist_gpu, idx, δ_gpu)
+@time hist_cpu!(hist, δ, idx)
+@CuArrays.time hist_gpu!(hist_gpu, δ_gpu, idx_gpu, MAX_THREADS=1024)
