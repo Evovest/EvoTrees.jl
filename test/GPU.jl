efa8f248bb8c77daf2e8172771b2060020194580
jeremiedb <jeremie_db@hotmail.com>
2020-03-05 20:19:49 -0500

    gpu tests

--- test/GPU.jl
+++ test/GPU.jl
@@ -49,10 +49,46 @@ idx_gpu = CuArray(idx)
 @CuArrays.time hist_gpu!(hist_gpu, δ_gpu, idx_gpu, MAX_THREADS=512)
 
 
+
+
+# GPU - apply along the features axis
+function kernel_y!(h::CuDeviceMatrix{T}, x::CuDeviceMatrix{T}, id) where {T<:AbstractFloat}
+    i = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    j = threadIdx().y + (blockIdx().y - 1) * blockDim().y
+    @inbounds if i <= size(id, 1) && j <= size(h, 2)
+        k = Base._to_linear_index(h, id[i,j], j)
+        CUDAnative.atomic_add!(pointer(h, k), x[i,j])
+    end
+    return
+end
+
+function hist_gpu_y!(h::CuMatrix{T}, x::CuMatrix{T}, id::CuMatrix{Int}; MAX_THREADS=256) where {T<:AbstractFloat}
+    thread_j = min(MAX_THREADS, size(id, 2))
+    thread_i = min(MAX_THREADS ÷ thread_j, size(h, 1))
+    threads = (thread_i, thread_j)
+    blocks = ceil.(Int, (size(id, 1), size(h, 2)) ./ threads)
+    CuArrays.@cuda blocks=blocks threads=threads kernel!(h, x, id)
+    return h
+end
+
+# blockdim: (THREADS,1)
+@CuArrays.time hist_gpu_y!(hist_gpu, δ_gpu, idx_gpu, MAX_THREADS=1024)
+
+function test(h)
+    t = zero(eltype(h))
+    for i in eachindex(h)
+        t += i
+    end
+    return t
+end
+test(hist_gpu)
+
+
+
 # GPU - 2-step approach
 function kernel_1!(h::CuDeviceMatrix{T}, x::CuDeviceMatrix{T}, id) where {T<:AbstractFloat}
     i = threadIdx().x + (blockIdx().x - 1) * blockDim().x
-    j = blockIdx().y * blockDim().y
+    j = blockIdx().y
     @inbounds if i <= size(id, 1) && j <= size(h, 2)
         k = Base._to_linear_index(h, id[i,j], j)
         CUDAnative.atomic_add!(pointer(h, k), x[i,j])
@@ -69,4 +105,5 @@ function hist_gpu!(h::CuMatrix{T}, x::CuMatrix{T}, id::CuMatrix{Int}; MAX_THREAD
     return h
 end
 
-@CuArrays.time hist_gpu!(hist_gpu, δ_gpu, idx_gpu, MAX_THREADS=500)
+# blockdim: (THREADS,1)
+@CuArrays.time hist_gpu!(hist_gpu, δ_gpu, idx_gpu, MAX_THREADS=256)
