0d523bf98ff54e1a4382bf0a7724977f613d17ec
jeremiedb <jeremie_db@hotmail.com>
2020-07-13 22:29:38 -0400

    update figures

--- src/find_split.jl
+++ src/find_split.jl
@@ -43,57 +43,6 @@ function update_set(set, best, x_bin)
     return left, right
 end
 
-
-function find_split_static!(hist_δ::Vector{SVector{L,T}}, hist_δ²::Vector{SVector{L,T}}, hist_𝑤::Vector{SVector{1,T}}, X_bin, δ::Vector{SVector{L,T}}, δ²::Vector{SVector{L,T}}, 𝑤::Vector{SVector{1,T}}, ∑δ::SVector{L,T}, ∑δ²::SVector{L,T}, ∑𝑤::SVector{1,T}, params::EvoTypes, info::SplitInfo{L,T,S}, edges::Vector{T}, set::Vector{S}) where {L,T,S}
-
-    # initialize histogram
-    hist_δ .*= 0.0
-    hist_δ² .*= 0.0
-    hist_𝑤 .*= 0.0
-
-    # initialize tracking
-    ∑δL = ∑δ * 0
-    ∑δ²L = ∑δ² * 0
-    ∑𝑤L = ∑𝑤 * 0
-    ∑δR = ∑δ
-    ∑δ²R = ∑δ²
-    ∑𝑤R = ∑𝑤
-
-    # build histogram
-    @inbounds for i in set
-        hist_δ[X_bin[i]] += δ[i]
-        hist_δ²[X_bin[i]] += δ²[i]
-        hist_𝑤[X_bin[i]] += 𝑤[i]
-    end
-
-    @inbounds for bin in 1:(length(hist_δ)-1)
-        ∑δL += hist_δ[bin]
-        ∑δ²L += hist_δ²[bin]
-        ∑𝑤L += hist_𝑤[bin]
-        ∑δR -= hist_δ[bin]
-        ∑δ²R -= hist_δ²[bin]
-        ∑𝑤R -= hist_𝑤[bin]
-
-        gainL, gainR = get_gain(params.loss, ∑δL, ∑δ²L, ∑𝑤L, params.λ), get_gain(params.loss, ∑δR, ∑δ²R, ∑𝑤R, params.λ)
-        gain = gainL + gainR
-
-        if gain > info.gain && ∑𝑤L[1] >= params.min_weight && ∑𝑤R[1] >= params.min_weight
-            info.gain = gain
-            info.gainL = gainL
-            info.gainR = gainR
-            info.∑δL = ∑δL
-            info.∑δ²L = ∑δ²L
-            info.∑𝑤L = ∑𝑤L
-            info.∑δR = ∑δR
-            info.∑δ²R = ∑δ²R
-            info.∑𝑤R = ∑𝑤R
-            info.cond = edges[bin]
-            info.𝑖 = bin
-        end
-    end
-    return
-end
-
 function update_hist!(hist_δ::Matrix{SVector{L,T}}, hist_δ²::Matrix{SVector{L,T}}, hist_𝑤::Matrix{SVector{1,T}},
     δ::Vector{SVector{L,T}}, δ²::Vector{SVector{L,T}}, 𝑤::Vector{SVector{1,T}},
     X_bin, node::TrainNode{L,T,S}) where {L,T,S}
