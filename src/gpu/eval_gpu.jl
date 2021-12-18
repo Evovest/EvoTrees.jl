397953feaeeb546c0d388489ccb704bbff954f79
jeremie.db <jeremie.db@evovest.com>
2021-12-18 15:10:54 -0500

    up

--- src/gpu/eval_gpu.jl
+++ src/gpu/eval_gpu.jl
@@ -1,60 +1,60 @@
 """
     MSE
 """
-function eval_mse_kernel!(eval::CuDeviceVector{T}, p::CuDeviceMatrix{T}, y::CuDeviceVector{T}) where {T <: AbstractFloat}
+function eval_mse_kernel!(eval::CuDeviceVector{T}, p::CuDeviceMatrix{T}, y::CuDeviceVector{T}, w::CuDeviceVector{T}) where {T<:AbstractFloat}
     i = threadIdx().x + (blockIdx().x - 1) * blockDim().x
     if i <= length(y)
-        @inbounds eval[i] = (p[1,i] - y[i])^2
+        @inbounds eval[i] = w[i] * (p[1, i] - y[i])^2
     end
     return nothing
 end
 
-function eval_metric(::Val{:mse}, eval::AbstractVector{T}, p::AbstractMatrix{T}, y::AbstractVector{T}, α; MAX_THREADS=1024) where T <: AbstractFloat
+function eval_metric(::Val{:mse}, eval::AbstractVector{T}, p::AbstractMatrix{T}, y::AbstractVector{T}, w::AbstractVector{T}, α; MAX_THREADS = 1024) where {T<:AbstractFloat}
     threads = min(MAX_THREADS, length(y))
     blocks = ceil(Int, length(y) / threads)
-    @cuda blocks = blocks threads = threads eval_mse_kernel!(eval, p, y)
+    @cuda blocks = blocks threads = threads eval_mse_kernel!(eval, p, y, w)
     CUDA.synchronize()
-    return mean(eval)
+    return sum(eval) / sum(w)
 end
 
 """
     Logloss
 """
-function eval_logloss_kernel!(eval::CuDeviceVector{T}, p::CuDeviceMatrix{T}, y::CuDeviceVector{T}) where {T <: AbstractFloat}
+function eval_logloss_kernel!(eval::CuDeviceVector{T}, p::CuDeviceMatrix{T}, y::CuDeviceVector{T}, w::CuDeviceVector{T}) where {T<:AbstractFloat}
     i = threadIdx().x + (blockIdx().x - 1) * blockDim().x
     if i <= length(y)
-        @inbounds pred = sigmoid(p[1,i])
-        @inbounds eval[i] = -y[i] * log(pred) + (y[i] - 1) * log(1 - pred)
+        @inbounds pred = sigmoid(p[1, i])
+        @inbounds eval[i] = w[i] * (-y[i] * log(pred) + (y[i] - 1) * log(1 - pred))
     end
     return nothing
 end
 
-function eval_metric(::Val{:logloss}, eval::AbstractVector{T}, p::AbstractMatrix{T}, y::AbstractVector{T}, α; MAX_THREADS=1024) where T <: AbstractFloat
+function eval_metric(::Val{:logloss}, eval::AbstractVector{T}, p::AbstractMatrix{T}, y::AbstractVector{T}, w::AbstractVector{T}, α; MAX_THREADS = 1024) where {T<:AbstractFloat}
     threads = min(MAX_THREADS, length(y))
     blocks = ceil(Int, length(y) / threads)
-    @cuda blocks = blocks threads = threads eval_logloss_kernel!(eval, p, y)
+    @cuda blocks = blocks threads = threads eval_logloss_kernel!(eval, p, y, w)
     CUDA.synchronize()
-    return mean(eval)
+    return sum(eval) / sum(w)
 end
 
 
 """
     Gaussian
 """
-function eval_gaussian_kernel!(eval::CuDeviceVector{T}, p::CuDeviceMatrix{T}, y::CuDeviceVector{T}) where {T <: AbstractFloat}
+function eval_gaussian_kernel!(eval::CuDeviceVector{T}, p::CuDeviceMatrix{T}, y::CuDeviceVector{T}, w::CuDeviceVector{T}) where {T<:AbstractFloat}
     i = threadIdx().x + (blockIdx().x - 1) * blockDim().x
     if i <= length(y)
-        @inbounds eval[i] = p[2,i] + (y[i] - p[1,i])^2 / (2 * exp(2 * p[2,i]))
+        @inbounds eval[i] = w[i] * (p[2, i] + (y[i] - p[1, i])^2 / (2 * exp(2 * p[2, i])))
     end
     return nothing
 end
 
-function eval_metric(::Val{:gaussian}, eval::AbstractVector{T}, p::AbstractMatrix{T}, y::AbstractVector{T}, α; MAX_THREADS=1024) where T <: AbstractFloat
+function eval_metric(::Val{:gaussian}, eval::AbstractVector{T}, p::AbstractMatrix{T}, y::AbstractVector{T}, w::AbstractVector{T}, α; MAX_THREADS = 1024) where {T<:AbstractFloat}
     threads = min(MAX_THREADS, length(y))
     blocks = ceil(Int, length(y) / threads)
-    @cuda blocks = blocks threads = threads eval_gaussian_kernel!(eval, p, y)
+    @cuda blocks = blocks threads = threads eval_gaussian_kernel!(eval, p, y, w)
     CUDA.synchronize()
-    return mean(eval)
+    return sum(eval) / sum(w)
 end
 
 
