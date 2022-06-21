8ff616bc9eecc5d2898f39610fca1917819140ec
jeremiedb <jeremie.bouchard@juliacomputing.com>
2022-06-21 00:19:22 -0400

    up

--- docs/make.jl
+++ docs/make.jl
@@ -4,16 +4,21 @@ using Documenter
 using EvoTrees
 
 pages = [
-    "Home" => "index.md",
+    "Source" => "https://github.com/Evovest/EvoTrees.jl",
+    "Introduction" => "index.md",
     "Examples" => "examples.md"]
 
 makedocs(
-    sitename = "EvoTrees.jl",
-    authors = "Jeremie Desgagne-Bouchard and contributors.",
-    format = Documenter.HTML(),
-    pages = pages,
-    modules = [EvoTrees],)
+    sitename="EvoTrees.jl",
+    authors="Jeremie Desgagne-Bouchard and contributors.",
+    format=Documenter.HTML(
+        sidebar_sitename=false,
+        edit_link = "main"
+    ),
+    pages=pages,
+    modules=[EvoTrees]
+)
 
-deploydocs(repo = "github.com/Evovest/EvoTrees.jl.git",
-    target = "build",
-    devbranch = "main")
+deploydocs(repo="github.com/Evovest/EvoTrees.jl.git",
+    target="build",
+    devbranch="main")
