b7085539f6856056b89602db7cb1d0a07ab7881e
jeremie.desgagne.bouchard <jeremie.desgagne.bouchard@gmail.com>
2022-11-04 23:01:15 -0400

    softmax fixes

--- experiments/benchmarks-softmax.jl
+++ experiments/benchmarks-softmax.jl
@@ -11,22 +11,24 @@ num_class = 5
 nthread = Base.Threads.nthreads()
 
 @info nthread
-loss_xgb = "reg:logistic"
+loss_xgb = "multi:softmax"
 metric_xgb = "mlogloss"
 metric_evo = :mlogloss
 
+
 # xgboost aprams
-# params_xgb = [
-#     "max_depth" => 5,
-#     "eta" => 0.05,
-#     "objective" => loss_xgb,
-#     "print_every_n" => 5,
-#     "subsample" => 0.5,
-#     "colsample_bytree" => 0.5,
-#     "tree_method" => "hist",
-#     "max_bin" => 64,
-# ]
-# metrics = [metric_xgb]
+params_xgb = [
+    "max_depth" => 5,
+    "eta" => 0.05,
+    "objective" => loss_xgb,
+    "print_every_n" => 5,
+    "subsample" => 0.5,
+    "colsample_bytree" => 0.5,
+    "tree_method" => "hist",
+    "max_bin" => 64,
+    "num_class" => num_class,
+]
+metrics = [metric_xgb]
 
 # EvoTrees params
 params_evo = EvoTreeClassifier(
@@ -50,27 +52,29 @@ num_feat = Int(100)
 x_train = rand(nobs, num_feat)
 y_train = rand(1:num_class, size(x_train, 1))
 
-# @info "xgboost train:"
-# @time m_xgb = xgboost(x_train, nrounds, label=y_train, param=params_xgb, metrics=metrics, nthread=nthread, silent=1);
-# @btime xgboost($x_train, $nrounds, label=$y_train, param=$params_xgb, metrics=$metrics, nthread=$nthread, silent=1);
-# @info "xgboost predict:"
-# @time pred_xgb = XGBoost.predict(m_xgb, x_train);
-# @btime XGBoost.predict($m_xgb, $x_train);
+@info "xgboost train:"
+@time m_xgb = xgboost(x_train, nrounds, label=y_train .- 1 , param=params_xgb, metrics=metrics, nthread=nthread, silent=1);
+@time m_xgb = xgboost(x_train, nrounds, label=y_train .- 1 , param=params_xgb, metrics=metrics, nthread=nthread, silent=1);
+# @btime xgboost($x_train, $nrounds, label=$y_train .- 1, param=$params_xgb, metrics=$metrics, nthread=$nthread, silent=1);
+@info "xgboost predict:"
+@time pred_xgb = XGBoost.predict(m_xgb, x_train);
+@btime XGBoost.predict($m_xgb, $x_train);
 
 @info "evotrees train CPU:"
 params_evo.device = "cpu"
 @time m_evo = fit_evotree(params_evo; x_train, y_train, x_eval=x_train, y_eval=y_train, metric=metric_evo, print_every_n=100);
-@btime fit_evotree($params_evo; x_train=$x_train, y_train=$y_train, x_eval=$x_train, y_eval=$y_train, metric=metric_evo);
-@btime fit_evotree($params_evo; x_train=$x_train, y_train=$y_train);
+@time m_evo = fit_evotree(params_evo; x_train, y_train, x_eval=x_train, y_eval=y_train, metric=metric_evo, print_every_n=100);
+# @btime fit_evotree($params_evo; x_train=$x_train, y_train=$y_train, x_eval=$x_train, y_eval=$y_train, metric=metric_evo);
+# @btime fit_evotree($params_evo; x_train=$x_train, y_train=$y_train);
 @info "evotrees predict CPU:"
 @time pred_evo = EvoTrees.predict(m_evo, x_train);
 @btime EvoTrees.predict($m_evo, $x_train);
 
 @info "evotrees train GPU:"
 params_evo.device = "gpu"
-@time m_evo_gpu = fit_evotree(params_evo; x_train, y_train);
 @time m_evo = fit_evotree(params_evo; x_train, y_train, x_eval=x_train, y_eval=y_train, metric=metric_evo, print_every_n=100);
-@btime fit_evotree($params_evo; x_train=$x_train, y_train=$y_train, x_eval=$x_train, y_eval=$y_train, metric=metric_evo);
+@time m_evo = fit_evotree(params_evo; x_train, y_train, x_eval=x_train, y_eval=y_train, metric=metric_evo, print_every_n=100);
+# @btime fit_evotree($params_evo; x_train=$x_train, y_train=$y_train, x_eval=$x_train, y_eval=$y_train, metric=metric_evo);
 @info "evotrees predict GPU:"
 @time pred_evo = EvoTrees.predict(m_evo_gpu, x_train);
 @btime EvoTrees.predict($m_evo_gpu, $x_train);
