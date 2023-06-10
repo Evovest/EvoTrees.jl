19af85c11b503d3632bbdbc2c1e40c49bc7477b1
jeremie <jeremie.desgagne.bouchard@gmail.com>
2023-06-10 11:29:05 -0400

    debug

---
+++ experiments/debug-softmax-nan.jl
@@ -0,0 +1,16 @@
+using Revise
+using Statistics
+using StatsBase: sample
+using EvoTrees
+using Random: seed!
+
+seed = 12
+seed!(seed)
+x_train = Array([
+   sin.(1:1000) rand(1000)
+   100 .* cos.(1:1000) rand(1000) .+ 1 
+]);
+y_train = repeat(1:2; inner = 1000);
+params1 = EvoTreeClassifier(; T = Float32, max_depth=3, rowsample  = 0.5, nrounds = 13, eta = 1, rng=seed)
+model = fit_evotree(params1; x_train, y_train)
+preds = EvoTrees.predict(model, x_train)[:, 1]
