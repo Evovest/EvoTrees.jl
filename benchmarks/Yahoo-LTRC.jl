b399207b025dc37ded5a7c806ae5aecb00821d1b
jeremie.desgagne.bouchard <jeremie.desgagne.bouchard@gmail.com>
2023-10-01 22:05:37 -0400

    ranking tests

--- benchmarks/Yahoo-LTRC.jl
+++ benchmarks/Yahoo-LTRC.jl
@@ -59,9 +59,25 @@ x_train = dtrain[:x][:, .!drop_cols]
 x_eval = deval[:x][:, .!drop_cols]
 x_test = dtest[:x][:, .!drop_cols]
 
+# x_train_miss = x_train .== 0
+# x_eval_miss = x_eval .== 0
+# x_test_miss = x_test .== 0
+
+# x_train[x_train.==0] .= 0.5
+# x_eval[x_eval.==0] .= 0.5
+# x_test[x_test.==0] .= 0.5
+
+# x_train = hcat(x_train, x_train_miss)
+# x_eval = hcat(x_eval, x_eval_miss)
+# x_test = hcat(x_test, x_test_miss)
+
 q_train = dtrain[:q]
 q_eval = deval[:q]
 q_test = dtest[:q]
+
+y_train = dtrain[:y];
+y_eval = deval[:y];
+y_test = dtest[:y];
 
 #####################################
 # mse regression
@@ -98,12 +114,12 @@ p_test = m_mse(x_test);
 test_df = DataFrame(p=p_test, y=y_test, q=q_test)
 test_df_agg = combine(groupby(test_df, "q"), ["p", "y"] => ndcg => "ndcg")
 ndcg_test = round(mean(test_df_agg.ndcg), sigdigits=5)
-@info "ndcg_test MSE" ndcg_test
+@info "MSE - test data - MSE model" mean((p_test .- y_test) .^ 2)
+@info "NDCG - test data - MSE model" ndcg_test
 
 #####################################
 # logistic regression
 #####################################
-
 max_rank = 4
 y_train = dtrain[:y] ./ max_rank
 y_eval = deval[:y] ./ max_rank
@@ -145,49 +161,59 @@ p_test = m_logloss(x_test);
 test_df = DataFrame(p=p_test, y=y_test, q=q_test)
 test_df_agg = combine(groupby(test_df, "q"), ["p", "y"] => ndcg => "ndcg")
 ndcg_test = round(mean(test_df_agg.ndcg), sigdigits=5)
-@info "ndcg_test LogLoss" ndcg_test
-
+@info "NDCG - test data - LogLoss model" ndcg_test
 
 #####################################
 # logistic regression on DataFrame
 #####################################
+target_name = "y"
 
 df_train = DataFrame(x_train, :auto)
-df_train.y = dtrain[:y]
+df_train.y = dtrain[:y] ./ 4
 df_train.q = dtrain[:q]
 
 df_eval = DataFrame(x_eval, :auto)
-df_eval.y = deval[:y]
+df_eval.y = deval[:y] ./ 4
 df_eval.q = deval[:q]
 
 df_test = DataFrame(x_test, :auto)
-df_test.y = dtest[:y]
+df_test.y = dtest[:y] ./ 4
 df_test.q = dtest[:q]
 
 function rank_target_norm(y::AbstractVector)
     out = similar(y)
     if minimum(y) == maximum(y)
-        # out .= 0.75
-        out .= 0.75
+        out .= 0.5
     else
-        # out .= (y .- minimum(y)) ./ (maximum(y) - minimum(y))
-        out .= 0.5 .* (y .- minimum(y)) ./ (maximum(y) - minimum(y)) .+ 0.5
-
+        out .= (y .- minimum(y)) ./ (maximum(y) - minimum(y))
     end
     return out
 end
 
-df_train = transform!(
-    groupby(df_train, "q"),
-    "y" => rank_target_norm => "y")
+function percent_rank(x::AbstractVector{T}) where {T}
+    return tiedrank(x) / (length(x) + 1)
+end
 
-df_eval = transform!(
-    groupby(df_eval, "q"),
-    "y" => rank_target_norm => "y")
+feature_names_raw = setdiff(names(df_train), ["y", "q"])
+feature_names_rel = feature_names_raw .* "_rel"
 
-df_test = transform!(
-    groupby(df_test, "q"),
-    "y" => rank_target_norm => "y")
+transform!(df_train, feature_names_raw .=> percent_rank .=> feature_names_rel)
+transform!(df_eval, feature_names_raw .=> percent_rank .=> feature_names_rel)
+transform!(df_test, feature_names_raw .=> percent_rank .=> feature_names_rel)
+
+feature_names = setdiff(names(df_train), ["y", "q"])
+
+# df_train = transform!(
+#     groupby(df_train, "q"),
+#     "y" => rank_target_norm => "y")
+
+# df_eval = transform!(
+#     groupby(df_eval, "q"),
+#     "y" => rank_target_norm => "y")
+
+# df_test = transform!(
+#     groupby(df_test, "q"),
+#     "y" => rank_target_norm => "y")
 
 minimum(df_eval.y)
 maximum(df_eval.y)
@@ -195,7 +221,7 @@ maximum(df_eval.y)
 config = EvoTreeRegressor(
     nrounds=6000,
     loss=:logloss,
-    eta=0.005,
+    eta=0.01,
     nbins=64,
     max_depth=11,
     rowsample=0.9,
@@ -205,8 +231,8 @@ config = EvoTreeRegressor(
 @time m_logloss_df, logger_logloss_df = fit_evotree(
     config,
     df_train;
-    target_name="y",
-    fnames=setdiff(names(df_train), ["y", "q"]),
+    target_name,
+    fnames=feature_names_raw,
     deval=df_eval,
     early_stopping_rounds=200,
     print_every_n=50,
@@ -214,14 +240,9 @@ config = EvoTreeRegressor(
     return_logger=true
 );
 
-# use the original y since NDCG is scale sensitive
-y_train = dtrain[:y]
-y_eval = deval[:y]
-y_test = dtest[:y]
-
 m_logloss_df.info
 p_test_df = m_logloss_df(df_test);
-p_test_mat = m_logloss_df(x_test);
+# p_test_mat = m_logloss_df(x_test);
 
 EvoTrees.importance(m_logloss_df)
 
@@ -229,4 +250,6 @@ p_test = m_logloss_df(df_test);
 test_df = DataFrame(p=p_test, y=dtest[:y], q=dtest[:q])
 test_df_agg = combine(groupby(test_df, "q"), ["p", "y"] => ndcg => "ndcg")
 ndcg_test = mean(test_df_agg.ndcg)
-@info "ndcg_test LogLoss DF" ndcg_test
+# ndcg_test = 0.8022558972243291
+# ndcg_test = 0.8020754563069513
+@info "NDCG - test data - LogLoss DF model" ndcg_test
