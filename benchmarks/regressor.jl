07da6cc2140129f1177a40075a08a5d7e2ae6456
jeremie <jeremie.desgagne.bouchard@gmail.com>
2023-11-09 20:14:51 -0500

    up

--- benchmarks/regressor.jl
+++ benchmarks/regressor.jl
@@ -125,7 +125,7 @@ device = "cpu"
 # @time m, cache = EvoTrees.init(params_evo, x_train, y_train);
 # @time m, cache = EvoTrees.init(params_evo, x_train, y_train);
 # @info "train - no eval"
-# @time m_evo = fit_evotree(params_evo; x_train, y_train, device, verbosity, print_every_n=100);
+@time m_evo = fit_evotree(params_evo; x_train, y_train, device, verbosity, print_every_n=100);
 # @time m_evo = fit_evotree(params_evo; x_train, y_train, device, verbosity, print_every_n=100);
 @info "train - eval"
 @time m_evo = fit_evotree(params_evo; x_train, y_train, x_eval=x_train, y_eval=y_train, metric=metric_evo, device, verbosity, print_every_n=100);
