123c0fad18df16ebf6535e1a242d73ab27359346
jeremie.db <jeremie.db@evovest.com>
2025-03-07 14:56:57 -0500

    up

--- experiments/issue-280/diagnosis.jl
+++ experiments/issue-280/diagnosis.jl
@@ -33,6 +33,7 @@ config = EvoTreeRegressor(;
 @info "fit"
 # depth 6: ~1.0 sec
 # depth 11: 14.466424 seconds (28.66 M allocations: 2.271 GiB, 5.09% gc time)
+# depth 11 laptop: 16.334244 seconds (27.06 M allocations: 2.154 GiB, 6.40% gc time)
 @time m = EvoTrees.fit(config, dtrain; target_name)
 # @profview EvoTrees.fit(config, dtrain; target_name)
 
