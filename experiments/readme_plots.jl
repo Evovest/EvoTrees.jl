99af060eb62e31f8a1aef7ccd1011e47b7f2b878
jeremie.desgagne.bouchard <jeremie.desgagne.bouchard@gmail.com>
2025-09-16 09:35:20 -0400

    bench

--- experiments/readme_plots.jl
+++ experiments/readme_plots.jl
@@ -182,16 +182,16 @@ config = EvoTreeRegressor(;
     device=_device
 )
 
-# @time model = fit(
-#     config;
-#     x_train,
-#     y_train,
-#     x_eval,
-#     y_eval,
-#     print_every_n=25,
-# );
-# @time pred_train_mae = model(x_train; device=_device)
-# sqrt(mean((pred_train_mae .- y_train) .^ 2))
+@time model = fit(
+    config;
+    x_train,
+    y_train,
+    x_eval,
+    y_eval,
+    print_every_n=25,
+);
+@time pred_train_mae = model(x_train; device=_device)
+sqrt(mean((pred_train_mae .- y_train) .^ 2))
 
 ###########################################
 # plot
