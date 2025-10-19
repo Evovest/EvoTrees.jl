bff2ca60f617da987ae8e1c251d6a2ed75400bff
jeremie.desgagne.bouchard <jeremie.desgagne.bouchard@gmail.com>
2025-10-19 14:54:47 -0400

    bench

--- benchmarks/regressor.jl
+++ benchmarks/regressor.jl
@@ -3,13 +3,13 @@ using DataFrames
 using CSV
 using Statistics
 using StatsBase: sample
-using XGBoost
+# using XGBoost
 using EvoTrees
 using BenchmarkTools
 using Random: seed!
 
 run_evo = true
-run_xgb = true
+run_xgb = false
 nrounds = 200
 
 loss = :mse
@@ -17,8 +17,8 @@ tree_type = "binary"
 T = Float64
 nthreads = Base.Threads.nthreads()
 
-device_list = [:cpu, :gpu]
-# device_list = [:gpu]
+# device_list = [:cpu, :gpu]
+device_list = [:gpu]
 
 nobs_list = Int.([1e5, 1e6, 1e7])
 # nobs_list = Int.([1e5])
