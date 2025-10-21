9e07b6816e75b29498f0866d0332b714bb0b36a0
jeremie.desgagne.bouchard <jeremie.desgagne.bouchard@gmail.com>
2025-10-21 09:01:50 -0400

    reproducible gpu

--- benchmarks/regressor.jl
+++ benchmarks/regressor.jl
@@ -20,8 +20,8 @@ nthreads = Base.Threads.nthreads()
 # device_list = [:cpu, :gpu]
 device_list = [:gpu]
 
-# nobs_list = Int.([1e5, 1e6, 1e7])
-nobs_list = Int.([1e6])
+nobs_list = Int.([1e5, 1e6, 1e7])
+# nobs_list = Int.([1e6])
 
 nfeats_list = [10, 100]
 # nfeats_list = [10]
