f163c775f508d97026aa84b666ed34b878b9c2ac
Sebastian Stock <42280794+sostock@users.noreply.github.com>
2025-12-09 16:20:23 +0100

    Fix a warning when constructing UnivariateFinite with CategoricalArrays v1 (#314)
    * Fix a warning when constructing UnivariateFinite
    
    * Restrict to CategoricalArrays v1

--- src/MLJ.jl
+++ src/MLJ.jl
@@ -52,7 +52,7 @@ end
 
 function predict(::EvoTreeClassifier, fitresult::EvoTree, A)
   pred = predict(fitresult, A)
-  return MMI.UnivariateFinite(fitresult.info[:target_levels], pred, pool=missing, ordered=fitresult.info[:target_isordered])
+  return MMI.UnivariateFinite(fitresult.info[:target_levels], pred)
 end
 
 function predict(::EvoTreeCount, fitresult::EvoTree, A)
