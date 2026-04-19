0dbc3a02e14a7a58c73f66291a6c2e4a50c4d406
jeremie.db <jeremie.db@evovest.com>
2026-04-19 16:55:42 -0400

    up

--- docs/quarto-build.jl
+++ docs/quarto-build.jl
@@ -1,12 +1,12 @@
 const DOCS_DIR = @__DIR__
 
 qmd_files = [
-    "tutorials/offset-usage.qmd",
+    joinpath("tutorials", "offset-usage.qmd"),
 ]
 
-for rel in qmd_files
-    src = joinpath(DOCS_DIR, "quarto", rel)
-    out = joinpath(DOCS_DIR, "src", dirname(rel))
-    @info "quarto render" file=src output_dir=out
+for doc in qmd_files
+    src = joinpath(DOCS_DIR, "quarto", doc)
+    out = joinpath(DOCS_DIR, "src")
+    @info "quarto render" doc output_dir = out
     run(`quarto render $src --output-dir $out`)
 end
