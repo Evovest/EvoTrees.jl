85e244685695a5af9cf55551c3c23058c7fbe948
jeremie.db <jeremie.db@evovest.com>
2026-08-13 06:17:03 -0400

    up benchmarks

--- benchmarks/regressor.jl
+++ benchmarks/regressor.jl
@@ -12,7 +12,7 @@ run_evo = true
 run_xgb = false
 nrounds = 200
 
-loss = :mse
+loss = :gaussian_mle # mse | gaussian_mle
 tree_type = :binary
 T = Float32
 nthreads = Base.Threads.nthreads()
@@ -55,7 +55,8 @@ for _device in device_list
                     @info "EvoTrees"
                     loss == :mse ? metric = :mae : metric = loss
 
-                    params_evo = EvoTreeRegressor(;
+                    # EvoTreeRegressor | EvoTreeMLE
+                    params_evo = EvoTreeMLE(;
                         loss,
                         metric,
                         nrounds,
@@ -132,6 +133,6 @@ for _device in device_list
         end
     end
     select!(df, Cols(:device, :nobs, :nfeats, :max_depth, r"train_", r"infer_"))
-    path = joinpath(@__DIR__, "results", "regressor-$_device.csv")
+    path = joinpath(@__DIR__, "results", "regressor-$loss-$_device.csv")
     CSV.write(path, df)
 end
